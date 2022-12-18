3cc43c4513919507093515bc775e6df6dc69977b
Jackk-Doe <dodo-stv@hotmail.com>
2022-12-18 17:06:21 +1300

    Change : how the server is run, in main.py

--- src/post_app/main.py
+++ src/post_app/main.py
@@ -4,5 +4,11 @@ import envs as _envs
 import server as _server
 
 if __name__ == '__main__':
-    # Start a Async gRPC Server
-    asyncio.run(_server.run_server())
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(_server.run_server())
+    except KeyboardInterrupt:
+        print("\nCTRL + C key detected...")
+        print("Shutted down Product server...")
+    finally:
+        loop.close()

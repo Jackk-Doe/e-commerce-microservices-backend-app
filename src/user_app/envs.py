28876bd200104aea55f0bf4705ed893ee5eba24e
Jackk-Doe <dodo-stv@hotmail.com>
2022-12-23 16:35:05 +1300

    Update : JWT signing key

--- src/user_app/envs.py
+++ src/user_app/envs.py
@@ -12,3 +12,6 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Application mode (e.g. dev, deploy, test). Default is set to dev mode
 MODE = os.getenv('MODE', 'dev')
+
+# This variable is used to sign a User.id, to JWT
+JWT_SERCRET = os.getenv('JWT_SERCRET')

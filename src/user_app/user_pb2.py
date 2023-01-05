5607f454a77ddb6ccc1296dbed13f650e25e0e08
Jackk-Doe <dodo-stv@hotmail.com>
2023-01-05 12:34:36 +0700

    Generate : updated new user pb files, User app

--- src/user_app/user_pb2.py
+++ src/user_app/user_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nuser.proto\x12\x04user\"\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\"\x16\n\x05Token\x12\r\n\x05value\x18\x01 \x01(\t\"?\n\x0eUserSignUpForm\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x10\n\x08password\x18\x03 \x01(\t\"0\n\rUserLogInForm\x12\r\n\x05\x65mail\x18\x01 \x01(\t\x12\x10\n\x08password\x18\x02 \x01(\t\"5\n\x07UserDTO\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\r\n\x05token\x18\x03 \x01(\t\"I\n\x0fInternalUserDTO\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\r\n\x05token\x18\x03 \x01(\t\x12\n\n\x02id\x18\x04 \x01(\t2\x9f\x02\n\x04User\x12+\n\x05LogIn\x12\x13.user.UserLogInForm\x1a\r.user.UserDTO\x12-\n\x06SignUp\x12\x14.user.UserSignUpForm\x1a\r.user.UserDTO\x12#\n\x05GetMe\x12\x0b.user.Token\x1a\r.user.UserDTO\x12\x1e\n\x05GetId\x12\x0b.user.Token\x1a\x08.user.Id\x12=\n\x17InternalGetUserViaToken\x12\x0b.user.Token\x1a\x15.user.InternalUserDTO\x12\x37\n\x14InternalGetUserViaId\x12\x08.user.Id\x1a\x15.user.InternalUserDTOb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\nuser.proto\x12\x04user\"\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\"\x16\n\x05Token\x12\r\n\x05value\x18\x01 \x01(\t\"?\n\x0eUserSignUpForm\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\x10\n\x08password\x18\x03 \x01(\t\"0\n\rUserLogInForm\x12\r\n\x05\x65mail\x18\x01 \x01(\t\x12\x10\n\x08password\x18\x02 \x01(\t\"&\n\x07UserDTO\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\">\n\x10UserDTOWithToken\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\r\n\x05token\x18\x03 \x01(\t\"I\n\x0fInternalUserDTO\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\r\n\x05\x65mail\x18\x02 \x01(\t\x12\r\n\x05token\x18\x03 \x01(\t\x12\n\n\x02id\x18\x04 \x01(\t2\xb1\x02\n\x04User\x12\x34\n\x05LogIn\x12\x13.user.UserLogInForm\x1a\x16.user.UserDTOWithToken\x12\x36\n\x06SignUp\x12\x14.user.UserSignUpForm\x1a\x16.user.UserDTOWithToken\x12#\n\x05GetMe\x12\x0b.user.Token\x1a\r.user.UserDTO\x12\x1e\n\x05GetId\x12\x0b.user.Token\x1a\x08.user.Id\x12=\n\x17InternalGetUserViaToken\x12\x0b.user.Token\x1a\x15.user.InternalUserDTO\x12\x37\n\x14InternalGetUserViaId\x12\x08.user.Id\x1a\x15.user.InternalUserDTOb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'user_pb2', globals())
@@ -29,9 +29,11 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _USERLOGINFORM._serialized_start=130
   _USERLOGINFORM._serialized_end=178
   _USERDTO._serialized_start=180
-  _USERDTO._serialized_end=233
-  _INTERNALUSERDTO._serialized_start=235
-  _INTERNALUSERDTO._serialized_end=308
-  _USER._serialized_start=311
-  _USER._serialized_end=598
+  _USERDTO._serialized_end=218
+  _USERDTOWITHTOKEN._serialized_start=220
+  _USERDTOWITHTOKEN._serialized_end=282
+  _INTERNALUSERDTO._serialized_start=284
+  _INTERNALUSERDTO._serialized_end=357
+  _USER._serialized_start=360
+  _USER._serialized_end=665
 # @@protoc_insertion_point(module_scope)
